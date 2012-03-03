a5894b3bd1aa6fe86262e2830fc2c2cbc14c2d68
Elomar Souza <elomar@elomar.me>
2012-03-03 01:19:15 -0300

    Finaliza páginas estáticas

--- spec/requests/paginas_spec.rb
+++ spec/requests/paginas_spec.rb
@@ -8,6 +8,11 @@ describe "Páginas" do
       visit "/paginas/index"
       page.should have_content("MicroBlog")
     end
+
+    it "Deve ter o título 'Início'" do
+      visit '/paginas/index'
+      page.should have_selector('title', text: "MicroBlog IT | Início")
+    end
   end
 
   describe "Ajuda" do
@@ -15,12 +20,22 @@ describe "Páginas" do
       visit "/paginas/ajuda"
       page.should have_content("Ajuda")
     end
+
+    it "Deve ter o título 'Ajuda'" do
+      visit '/paginas/ajuda'
+      page.should have_selector('title', text: "MicroBlog IT | Ajuda")
+    end
   end
 
   describe "Sobre" do
     it "deve ter o conteúdo 'Sobre'" do
       visit "/paginas/sobre"
       page.should have_content("Sobre")
+    end
+
+    it "Deve ter o título 'Sobre'" do
+      visit '/paginas/sobre'
+      page.should have_selector('title', text: "MicroBlog IT | Sobre")
     end
   end
 end
