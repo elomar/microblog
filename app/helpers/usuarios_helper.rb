bf5bed20c48f1ebfba4cc5ca78d059651f59c195
Elomar Souza <elomar@elomar.me>
2012-03-09 23:30:18 -0300

    Mostra gravatar

--- app/helpers/usuarios_helper.rb
+++ app/helpers/usuarios_helper.rb
@@ -1,2 +1,7 @@
 module UsuariosHelper
+  def gravatar_for(usuario)
+    gravatar_id = Digest::MD5::hexdigest(usuario.email.downcase)
+    gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}.png"
+    image_tag(gravatar_url, alt: usuario.nome, class: "gravatar")
+  end
 end
