fa8f40b2d926dc6c77c8458ecece8c13b361d80a
Elomar Souza <elomar@elomar.me>
2012-03-10 11:06:08 -0300

    Trata criação de usuário

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -6,4 +6,13 @@ class UsuariosController < ApplicationController
   def new
     @usuario = Usuario.new
   end
+
+  def create
+    @usuario = Usuario.new(params[:usuario])
+    if @usuario.save
+      redirect_to @usuario, notice: "Bem vindo ao site!"
+    else
+      render 'new'
+    end
+  end
 end
