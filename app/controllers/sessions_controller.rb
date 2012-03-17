980714fa9d30d199c73711accdb6e45335cd4575
Elomar Souza <elomar@elomar.me>
2012-03-17 00:30:51 -0300

    Tenta mostrar falha de login com flash

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
+# encoding: utf-8
+
 class SessionsController < ApplicationController
   def new
+  end
+
+  def create
+    usuario = Usuario.find_by_email(params[:session][:email])
+    if usuario && user.authenticate(params[:session][:password])
+      
+    else
+      flash[:alert] = "Usuário/senha inválidos!"
+      render 'new'      
+    end
   end
 end
