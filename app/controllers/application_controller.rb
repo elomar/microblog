3755c28bfc14a771e8dcf39a072ebd0a2cc9cc93
Elomar Souza <elomar@elomar.me>
2012-03-17 01:18:31 -0300

    Mostra links baseado em usuário atual

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  helper_method :logado?, :usuario_logado
+
   protected
 
     def login(usuario)
       cookies.permanent[:remember_token] = usuario.remember_token
       @usuario_logado = usuario
+    end
+
+    def logado?
+      usuario_logado.present?
     end
 
     def usuario_logado
