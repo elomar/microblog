2b3f355dfb5fd88591713d2b70eaaa893523079d
Elomar Souza <elomar@elomar.me>
2012-03-17 01:39:33 -0300

    Movendo métodos de sessão pra um helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,24 @@
 module SessionsHelper
+  def login(usuario)
+    cookies.permanent[:remember_token] = usuario.remember_token
+    @usuario_logado = usuario
+  end
+
+  def logout
+    usuario_logado = nil
+    cookies.delete(:remember_token)
+  end
+
+  def logado?
+    usuario_logado.present?
+  end
+
+  def usuario_logado
+    @usuario_logado ||= usuario_com_remember_token
+  end
+
+  def usuario_com_remember_token
+    remember_token = cookies[:remember_token]
+    Usuario.find_by_remember_token(remember_token) unless remember_token.nil?
+  end
 end
