4c16a3bdbffd1bb5202e36b361c80312960e9e41
Elomar Souza <elomar@elomar.me>
2012-03-17 01:14:47 -0300

    Faz login funcionar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  protected
+
+    def login(usuario)
+      cookies.permanent[:remember_token] = usuario.remember_token
+      @usuario_logado = usuario
+    end
+
+    def usuario_logado
+      @usuario_logado ||= usuario_com_remember_token
+    end
+
+    def usuario_com_remember_token
+      remember_token = cookies[:remember_token]
+      Usuario.find_by_remember_token(remember_token) unless remember_token.nil?
+    end
 end
