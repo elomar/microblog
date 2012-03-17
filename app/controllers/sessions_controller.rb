34d7e5d511a91b8a2551df39be6b777825480075
Elomar Souza <elomar@elomar.me>
2012-03-17 00:57:45 -0300

    Cria estrutura de login válido

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,8 @@ class SessionsController < ApplicationController
   def create
     usuario = Usuario.find_by_email(params[:session][:email])
     if usuario && usuario.authenticate(params[:session][:password])
-      
+      login usuario
+      redirect_to usuario      
     else
       flash.now[:alert] = "Usuário/senha inválidos!"
       render 'new'      
