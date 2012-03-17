5e82338637028a554a21ac2e96f8d1ee6d8698fc
Elomar Souza <elomar@elomar.me>
2012-03-17 00:18:12 -0300

    Testa login

--- spec/requests/autenticacao_spec.rb
+++ spec/requests/autenticacao_spec.rb
@@ -11,5 +11,26 @@ describe "Autenticação" do
 
     it { should have_selector('h1',    text: 'Entre no site') }
     it { should have_selector('title', text: 'Entre no site') }
+
+    describe "com informação inválida" do
+      before { click_button "Entre no site" }
+
+      it { should have_selector('title', text: 'Entre no site') }
+      it { should have_selector('div.alert.alert-error', text: 'Inválido') }
+    end
+
+    describe "com informação válida" do
+      let(:usuario) { FactoryGirl.create(:usuario) }
+      before do
+        fill_in "Email", with: usuario.email
+        fill_in "Senha", with: usuario.senha
+        click_button "Entre no site"
+      end
+
+      it { should have_selector('title', text: usuario.name) }
+      it { should have_link('Perfil', href: usuario_path(usuario)) }
+      it { should have_link('Sair', href: logout_path) }
+      it { should_not have_link('Entre no site', href: login_path) }
+    end
   end
 end
